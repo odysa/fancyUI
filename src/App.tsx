476096910578cdabf2189613c692c613e8f806be
Chengxu Bian <piensengxv@gmail.com>
2020-07-05 11:33:54 +0800

    initialize and add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Button, { ButtonType, ButtonSize } from "./components/Button/button";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+          Hello
+        </Button>
+        <Button
+          btnType={ButtonType.Link}
+          size={ButtonSize.Small}
+          href={"www.baidu.com"}
         >
-          Learn React
-        </a>
+          Link Test
+        </Button>
+        <Button
+          btnType={ButtonType.Primary}
+          size={ButtonSize.Large}
+        >
+          Large Button
+        </Button>
       </header>
     </div>
   );
